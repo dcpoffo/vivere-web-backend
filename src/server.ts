abf72235e8d12a16d093840c7a30af2bb771a6f1
dcpoffo@gmail.com <dcpoffo@gmail.com>
2024-11-24 20:03:06 -0300

    configuração de cors

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,19 @@ const app = Fastify({ logger: true })
 
 const start = async () => {
 
-    await app.register(cors);
+    //await app.register(cors);
+    await app.register(cors, {
+        origin: '*', // Substitua pela URL do seu frontend
+        methods: [ 'GET', 'POST', 'PUT', 'DELETE' ], // Métodos permitidos
+        allowedHeaders: [ 'Content-Type', 'Authorization' ], // Cabeçalhos permitidos
+        credentials: true, // Se você precisar enviar cookies (como tokens de sessão)
+    });
+
     await app.register(routes)
 
     try {
-        await app.listen({ port: 3333})
+        await app.listen({ port: 3333 });
+        console.log('Servidor rodando na porta 3333');
     } catch (err) {
         process.exit(1)
     }
